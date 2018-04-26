5727c1d30dfbc8532b43dc4090a938deb5836fd5
huwenjian <hwj_gz@163.com>
2018-04-26 10:54:39 +0800

    init lee-code project

---
+++ src/main/java/com/leecode/leecode/LeecodeApplication.java
@@ -0,0 +1,12 @@
+package com.leecode.leecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeecodeApplication.class, args);
+	}
+}
